0e816d412acfa325ec630cc7d9fd622483bc3fd6
sparkvilla <diego.villamaina@gmail.com>
2019-08-06 15:47:40 +0200

    corrected workflow

--- pygeoml/main.py
+++ pygeoml/main.py
@@ -19,12 +19,6 @@ def build_geo_df(dirpath_to_shapes):
         so.reproject('epsg:26912')
     return [sh.gdf for sh in shapeobjs]
 
-def build_features_lables(path_to_raster, list_of_gdfs, outdir):
-    a = Trainingdata.calc_xy(path_to_raster, *list_of_gdfs)
-    a.add_background_xy()
-    a.save_xy('Finalsubset',outdir)
-    return a
-
 if __name__ == "__main__":
 
     basedir = '/home/diego/work/dev/ess_diego/'
@@ -35,7 +29,14 @@ if __name__ == "__main__":
 
     calc_ndvi(inpRaster, outdir)
     shps_list = build_geo_df(inpShapes)
-    r1 = build_features_lables(inpRaster, shps_list, outdir)
-    r2 = build_features_lables(os.path.join(outdir,'ndvi.gtif'), shps_list, outdir)
-    r3 = r1.hstack_to_x(r2)
-    r3.save_xy('Finalxy',outdir)
+
+    td1 = Trainingdata.calc_xy(inpRaster, shps_list)
+    td1.add_background_xy()
+    td1.save_xy('base_extracted',outdir)
+
+    td2 = Trainingdata.calc_xy(os.path.join(outdir,'ndvi.gtif'), shps_list)
+    td2.add_background_xy()
+    td2.save_xy('ndvi_extracted',outdir)
+
+    td3 = td1.hstack_to_x(td2)
+    td3.save_xy('base_and_ndvi_extracted',outdir)
