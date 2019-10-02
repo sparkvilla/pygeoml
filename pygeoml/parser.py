017b385b319430f6f8d1a99144612fcd814b59b6
sparkvilla <diego.villamaina@gmail.com>
2019-10-02 14:37:54 +0200

    changed lookup table

--- pygeoml/parser.py
+++ pygeoml/parser.py
@@ -9,15 +9,15 @@ class Sentinel2:
             # T37MBN_20170718T075211_B02.jp2
             r'.*_(?P<date>[0-9]{8})'
             r'.*_(?P<band>B0(2|3|4|8))'
-            r'\.(?P<fext>jp2$)'
+            r'.*(?P<fext>jp2$)'
             )
 
     def __init__(self, dirpath):
         self.dirpath = dirpath
-        self.lookup = {'B02': 'blue',
-                       'B03': 'green',
-                       'B04': 'red',
-                       'B08': 'nir'}
+        self.lookup = {'B02': 'Sent2_B02', # BLUE
+                       'B03': 'Sent2_B03', # GREEN
+                       'B04': 'Sent2_B04', # RED
+                       'B08': 'Sent2_B08'} # NIR
         self.rfiles = []
         for path_to_f in os.listdir(self.dirpath):
             match = Sentinel2.PATTERN.match(os.path.basename(path_to_f))
@@ -29,7 +29,6 @@ class Sentinel2:
         self._verify_rfiles_not_empty()
         # sort by the Band number
         self.srfiles = sorted(self.rfiles)
-        self.fname = '_'.join((self.__class__.__name__, self.date, '.tif'))
 
     def __repr__(self):
         return 'Sentinel2({})'.format(self.dirpath)
@@ -40,14 +39,13 @@ class Sentinel2:
 
 if __name__ == '__main__':
 
-    basedir = '/home/diego/work/dev/ess_diego/'
+    basedir = '/home/diego/work/dev/ess_diego/Data_Diego'
     imgdir = os.path.join(
         basedir,
-        'Data_Diego/Sentinel/Sentinel_2017/S2B_MSIL1C_20171018T072859_N0205_R049_T37MBN_20171018T074738.SAFE/GRANULE/L1C_T37MBN_A003221_20171018T074738/IMG_DATA')
+        'S2B_MSIL2A_20190812T073619_N9999_R092_T37MBN_20190919T144441.SAFE/GRANULE/L2A_T37MBN_A012702_20190812T075555/IMG_DATA/R10m/')
 
     p = Sentinel2(imgdir)
     print(p.rfiles)
     print("")
     print(p.srfiles)
     print(p.date)
-    print(p.fname)
