cf2ef3ae47d65cb4ebfd5c3547836665c9c7b624
sparkvilla <diego.villamaina@gmail.com>
2019-10-22 14:19:58 +0200

    added Rasterhsp class for hyperspectral data

--- pygeoml/raster.py
+++ pygeoml/raster.py
@@ -5,13 +5,13 @@ import re
 #from parser import Sentinel2
 import rasterio
 import rasterio.plot
-from rasterio.plot import reshape_as_image
+from rasterio.plot import reshape_as_image, plotting_extent
 from rasterio.mask import mask
 from rasterio.merge import merge
 from rasterio.windows import Window
 import numpy as np
-
-from shapely.geometry import mapping
+import matplotlib.pyplot as plt
+from shapely.geometry import Point, Polygon, mapping
 
 
 class Raster:
@@ -51,22 +51,26 @@ class Raster:
         This is the order expected by image processing and visualization software;
         i.e. matplotlib, scikit-image, etc..
 
-        By default it load the entire raster, else one can specify a window.
         """
         # check whether to load the entire array
         if height is None:
             height = self.height
         if width is None:
             width = self.width
+        # check how many bands to load
         if bands is None:
             bands = [i for i in range(1, self.count+1)]
+        else:
+            # make sure bands is a list of indexes
+            if not isinstance(bands, list):
+                bands = [bands]
 
         with rasterio.open(self.path_to_raster) as dataset:
             img = dataset.read(bands, window=Window(col_off, row_off, width, height))
         return reshape_as_image(img)
 
     @classmethod
-    def load_rgb(cls, red_obj, green_obj, blue_obj, height, width, bands=[1], col_off=0,row_off=0):
+    def load_rgb(cls, red_obj, green_obj, blue_obj, height, width, bands=1, col_off=0,row_off=0):
 
         r = red_obj.load_as_arr(height, width, bands, col_off, row_off)
         g = green_obj.load_as_arr(height, width, bands, col_off, row_off)
@@ -74,9 +78,9 @@ class Raster:
 
 
         rgb_norm = np.empty((height,width,3), dtype=np.float32)
-        rgb_norm[:,:,0] = Raster.normalize(r[:,:,0])
-        rgb_norm[:,:,1] = Raster.normalize(g[:,:,0])
-        rgb_norm[:,:,2] = Raster.normalize(b[:,:,0])
+        rgb_norm[:,:,0] = Raster._normalize(r[:,:,0])
+        rgb_norm[:,:,1] = Raster._normalize(g[:,:,0])
+        rgb_norm[:,:,2] = Raster._normalize(b[:,:,0])
 
         # normalized RGB natural color composite
         return rgb_norm
@@ -96,7 +100,55 @@ class Raster:
         row,col = rasterio.transform.rowcol(self.transform, xs, ys)
         return row, col
 
+    def extract_pixel_value(self, gdf):
+        """
+        It returns pixel values at Point coordinates contained in gdf
 
+        args:
+            gdf -- Geopandas dataframe. A column named 'geometry' is expected
+        """
+        gdf_copy = gdf.copy()
+
+        geoms = gdf_copy.geometry.values
+
+        with rasterio.open(self.path_to_raster) as src:
+            pixel_values = np.array([]).reshape(0,src.count)
+            for index, geom in enumerate(geoms):
+                feature = [mapping(geom)]
+                #3D np array
+                out_image, out_transform = mask(src, feature, crop=True)
+                #2D np array
+                out_image_reshaped = out_image.reshape(-1, src.count)
+                pixel_values = np.vstack((pixel_values,out_image_reshaped))
+        return pixel_values
+
+    def get_raster_polygon(self):
+        """
+        It returns a Polygon Shapely object of the entire raster using
+        witdh and height
+        """
+
+        endrow = self.height
+        endcol = self.width
+
+        top_left = self.transform_to_coordinates(0,0)
+        bottom_left = self.transform_to_coordinates(endrow,0)
+        top_right = self.transform_to_coordinates(0,endcol)
+        bottom_right = self.transform_to_coordinates(endrow,endcol)
+        coords = [top_left, bottom_left, bottom_right, top_right]
+
+        return Polygon(coords)
+
+    def get_gdf_within(self, gdf):
+        """
+        Return a new geodataframe with Points only within the
+        Raster polygon
+        """
+        poly = self.get_raster_polygon()
+        # create a mask
+        mask = gdf.within(poly)
+        gdf_within = gdf.loc[mask]
+        return gdf_within
 
     def write_tiles(self, tile_size_x=50, tile_size_y=70): # To be finished
         with rasterio.open(self.path_to_raster) as dataset:
@@ -106,8 +158,40 @@ class Raster:
                     tile = dataset.read(window=Window(col, row, tile_size_x, tile_size_y))
                     yield tile.shape
 
+
     @classmethod
-    def load_ndvi(cls, red_obj, nir_obj, height, width, bands=[1], col_off=0, row_off=0, write=False):
+    def points_on_layer_plot(self, r_obj, layer_arr, gdf, **kwargs):
+
+        layer_endrow = layer_arr.shape[0]
+        layer_endcol = layer_arr.shape[1]
+        layer_poly = [r_obj.transform_to_coordinates(0,0), r_obj.transform_to_coordinates(layer_endrow,0),
+                      r_obj.transform_to_coordinates(layer_endrow,layer_endcol), r_obj.transform_to_coordinates(0,layer_endcol)]
+
+        # check for raster and arr coordinates
+        assert r_obj.get_raster_polygon() == Polygon(layer_poly), "Input array and raster must have same coordinates"
+
+        cmap, marker, markersize, color, label = kwargs.get('r_cmap',"pink"), \
+                                  kwargs.get('s_marker',"s"), \
+                                  kwargs.get('s_markersize',30), \
+                                  kwargs.get('s_color',"purple"), \
+                                  kwargs.get('s_label',"classname")
+
+        # Plotting
+        fig, ax = plt.subplots(figsize=(10, 10))
+
+        ax.imshow(layer_arr[:,:,0],
+                      # Set the spatial extent or else the data will not line up with your geopandas layer
+                      extent=plotting_extent(r_obj),
+                      cmap=cmap)
+        gdf.plot(ax=ax,
+                     marker=marker,
+                     markersize=markersize,
+                     color=color,
+                     label=label)
+        return ax
+
+    @classmethod
+    def calc_ndvi(cls, red_obj, nir_obj, height, width, bands=1, col_off=0, row_off=0, write=False):
         """
         Return a calculated ndvi 3D numpy array. The axis of the array have the order:
 
@@ -140,7 +224,7 @@ class Raster:
         return ndvi_arr
 
     @staticmethod
-    def normalize(arr):
+    def _normalize(arr):
         """Normalizes numpy arrays into scale 0.0 - 1.0"""
         array_min, array_max = arr.min(), arr.max()
         return ((arr - array_min)/(array_max - array_min))
@@ -199,44 +283,68 @@ class Rastermsp(Raster):
         rgb = self.load_as_arr(height, width, [red_idx,green_idx,blue_idx], col_off, row_off)
 
         rgb_norm = np.empty((height,width,3), dtype=np.float32)
-        rgb_norm[:,:,0] = self.normalize(rgb[:,:,0])
-        rgb_norm[:,:,1] = self.normalize(rgb[:,:,1])
-        rgb_norm[:,:,2] = self.normalize(rgb[:,:,2])
+        rgb_norm[:,:,0] = self._normalize(rgb[:,:,0])
+        rgb_norm[:,:,1] = self._normalize(rgb[:,:,1])
+        rgb_norm[:,:,2] = self._normalize(rgb[:,:,2])
 
         # normalized RGB natural color composite
         return rgb_norm
 
-# to be change with Rasterhsp()
-class Rasterobj():
+    @classmethod
+    def points_on_layer_plot(self, r_obj, arr, gdf, **kwargs):
+
+        layer_arr = arr[:,:,0]
+        layer_endrow = layer_arr.shape[0]
+        layer_endcol = layer_arr.shape[1]
+        layer_poly = [r_obj.transform_to_coordinates(0,0), r_obj.transform_to_coordinates(layer_endrow,0),
+                      r_obj.transform_to_coordinates(layer_endrow,layer_endcol), r_obj.transform_to_coordinates(0,layer_endcol)]
+
+        # check for raster and arr coordinates
+        assert r_obj.get_raster_polygon() == Polygon(layer_poly), "Input array and raster must have same coordinates"
+
+        cmap, marker, markersize, color, label = kwargs.get('r_cmap',"pink"), \
+                                  kwargs.get('s_marker',"s"), \
+                                  kwargs.get('s_markersize',30), \
+                                  kwargs.get('s_color',"purple"), \
+                                  kwargs.get('s_label',"classname")
+
+        # Plotting
+        fig, ax = plt.subplots(figsize=(10, 10))
+
+        ax.imshow(layer_arr[:,:,0],
+                      # Set the spatial extent or else the data will not line up with your geopandas layer
+                      extent=plotting_extent(r_obj),
+                      cmap=cmap)
+        gdf.plot(ax=ax,
+                     marker=marker,
+                     markersize=markersize,
+                     color=color,
+                     label=label)
+        return ax
+
+    def show_hist(self):
+        with rasterio.open(self.path_to_raster) as dataset:
+            rasterio.plot.show_hist(dataset.read([1,2,3,4]), bins=50, histtype='stepfilled', lw=0.0, stacked=False, alpha=0.3)
+
+class Rasterhsp(Raster):
 
     RED = 672
     NIR = 814
 
-    def __init__(self, path_to_raster, outdir=None):
+    def __init__(self, path_to_raster):
+        super().__init__(path_to_raster)
+        assert self.count > 1, '"Rasterhsp" class process multibands raster only, your raster has a single band. Try to use the "Raster" class instead.'
+        self.wl = None
+        self.band_wl = {}
 
-        self.path_to_raster = path_to_raster
-        self.outdir = outdir
-
-        with rasterio.open(path_to_raster) as dataset:
-            self.driver = dataset.driver
-            self.num_bands = dataset.count
-            self.desc = dataset.descriptions
-            self.crs = dataset.crs
-            self.meta = dataset.meta
-            self.height = self.meta['height']
-            self.width = self.meta['width']
-            self.band_wl = {}
-            self.red_band = None
-            self.nir_band = None
-            self.ndvi_band = None
-            self.wl = None
-
+    def map_band_wl(self):
+        ## This is case specific. It has to be replaced with a more general version. ##
         ## To be replaced with parser for specific driver ##
         if self.driver == 'ENVI':
             print("Found {} driver. Start band -> wl mapping...".format(self.driver))
             table = str.maketrans(dict.fromkeys("()"))
             # remap actual band number with wavelength
-            for idx in range(self.num_bands):
+            for idx in range(self.count):
                 l = self.desc[idx].translate(table).split()
                 key = idx+1 # start from 1
                 # To access the band number written in desc:
@@ -258,7 +366,7 @@ class Rasterobj():
         and return the corresponding key
 
         example:
-        >> print(Dataset._get_bands_at_wl({1:587.67,2:597.17,3:606.85}, 600))
+        >> print(Rasterhsp._get_idx_band_at_wl({1:587.67,2:597.17,3:606.85}, 600))
         >> 2
         """
         differences = {key:abs(wl-val) for key, val in mapping.items()}
@@ -279,69 +387,49 @@ class Rasterobj():
         arrs = [arr[i,:,:] for i in indices]
         return np.mean(arrs,axis=0)
 
-    def get_pixel_array(self, col_start):
+    def calc_ndvi(self, red_arr, nir_arr, bands=1, col_off=0, row_off=0, write=False):
         """
-        Get a one pixel window
+        Return a calculated ndvi 3D numpy array. The axis of the array have the order:
 
-        Return a 1D np array of len equal to self.wl
+        (height, width, bands) or
+        (rows, cols, channels)
 
-        """
-        pass
-
-    def get_red_and_nir_bands(self):
-        """
-        It fills the red and nir instance attributes with
-        the respective band, i.e. 2D np-array extracted from the raster
-        """
-        red_idx = Rasterobj._get_idx_band_at_wl(self.band_wl, self.RED)
-        nir_idx = Rasterobj._get_idx_band_at_wl(self.band_wl, self.NIR)
-        with rasterio.open(self.path_to_raster) as dataset:
-            # +1 -> indexing: numpy start at 0 rasterio at 1
-            self.red_band = dataset.read(red_idx)
-            self.nir_band = dataset.read(nir_idx)
-
-    def get_ndvi_band(self, write=False):
-        """
-        It fills the ndvi instance attribute with a 2D np-array
-        calculated the using the red and nir bands attributes.
+        This is the order expected by image processing and visualization software;
+        i.e. matplotlib, scikit-image, etc..
 
         When write=True the ndvi is saved to disk as ndvi.gtif
 
         """
-        if (isinstance(self.red_band,np.ndarray) & isinstance(self.nir_band,np.ndarray)):
-            np.seterr(divide='ignore', invalid='ignore')
-            self.ndvi_band = (self.nir_band.astype(float)-self.red_band.astype(float))/(self.nir_band.astype(float)+self.red_band.astype(float))
+        np.seterr(divide='ignore', invalid='ignore')
+        ndvi_arr = (nir_arr.astype(float)-red_arr.astype(float))/(nir_arr.astype(float)+red_arr.astype(float))
 
-            # write to disk
-            if write:
-                ndvi_meta = copy.deepcopy(self.meta)
-                ndvi_meta.update(count=1, dtype="float64", driver='GTiff')
-                ndvi_path = os.path.dirname(self.path_to_raster)
-                if self.outdir:
-                    ndvi_path = self.outdir
-                with rasterio.open(os.path.join(ndvi_path,'ndvi.gtif'), 'w', **ndvi_meta) as dst:
-                            dst.write(self.ndvi_band, 1)
-        else:
-            raise ValueError("Red and Nir band values are not correct. Try to call first 'get_red_and_nir_bands()' method.")
+        # write to disk
+        if write:
+            # grab and copy metadata of one of the two array
+            ndvi_meta = copy.deepcopy(self.meta)
+            ndvi_meta.update(count=1, dtype="float64", driver='GTiff')
+            ndvi_path = os.path.dirname(self.path_to_raster)
+            with rasterio.open(os.path.join(ndvi_path,'ndvi.gtif'), 'w', **ndvi_meta) as dst:
+                dst.descriptions = ['ndvi']
+                dst.write(ndvi_arr, 1)
+        return ndvi_arr
 
-    def get_raster_window(self, width, height, col_off=0,row_off=0):
+
+    def load_raster_profile(self, row, col):
         """
-        Load the raster as numpy array
+        Get a one pixel window along the stack
+
+        *********
+        params:
+            row -> row index
+            col -> col index
+
+        ********
+        return:
+            1D numpy array of len equal to self.wl
+
         """
-
-        with rasterio.open(self.path_to_raster) as dataset:
-            img = dataset.read(window=Window(col_off, row_off, width, height))
-        return img
-
-
-    def get_raster_tiles(self, tile_size_x=50, tile_size_y=70):
-
-        with rasterio.open(self.path_to_raster) as dataset:
-            for col in range(0, self.width, tile_size_x):
-                for row in range(0, self.height, tile_size_y):
-                    #print(row,col)
-                    tile = dataset.read(window=Window(col, row, tile_size_x, tile_size_y))
-                    yield tile.shape
+        return self.load_as_arr(height=1, width=1, col_off=col, row_off=row)[0,0,:]
 
 if __name__ == '__main__':
     basedir = '/home/diego/work/dev/ess_diego/'
