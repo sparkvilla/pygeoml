0127451a51d16567ef4426d463adf4dccb3e4f7d
sparkvilla <diego.villamaina@gmail.com>
2020-01-14 14:56:04 +0100

    added predict func to classify raster pixels

--- pygeoml/train.py
+++ pygeoml/train.py
@@ -9,22 +9,22 @@ import pandas as pd
 import numpy as np
 
 from shapely.geometry import mapping
-
+from pygeoml.utils import np_to_disk
 
 class Trainingdata:
     """
-    To Manipulate or generate
-    X features
-    y labels
+    Generate/modify:
+      X features
+      y labels
     used as training data for various ML algorithms.
     """
     def __init__(self, X, y):
         self.X = X
         self.y = y
 
-    def save_xy(self, rname, outdir):
-        np.save(os.path.join(outdir,rname+'_features.npy'),self.X)
-        np.save(os.path.join(outdir,rname+'_lables.npy'),self.y)
+    def save_xy(self, rpath, outdir):
+        np_to_disk(self.X, 'features', rpath, outdir)
+        np_to_disk(self.y, 'lables', rpath, outdir)
 
     def add_class_xy(self, class_name, class_value, n=80):
         """
@@ -47,6 +47,10 @@ class Trainingdata:
         final_x = np.hstack((self.X,other.X))
         return Trainingdata(final_x,self.y)
 
+    def build_str_to_int_map(self):
+        classnames = list(set(self.y))
+        self.str_int_map = {classnames[idx]:idx for idx in range(0,len(classnames))}
+
     @classmethod
     def to_df(cls, X, y):
         # check X feature and y labels have the same 0 dimension
@@ -64,7 +68,7 @@ class Trainingdata:
         return Trainingdata(X,y)
 
     @classmethod
-    def calc_xy(cls, path_to_raster, gdf, write=False, outdir=None):
+    def calc_xy(cls, r_obj, gdf, write=False, outdir=None):
         """
         Build the X feature matrix by extracting the pixel values from the raster
         at the point location.
@@ -77,6 +81,15 @@ class Trainingdata:
         Point1    val1 val2                     classname
         Point2                                  classname
 
+        ************
+
+        params:
+            r_obj -> An istance of the Raster class
+            gdf -> A geodataframe of point geometries
+            outdir -> full path to output directory
+
+        return:
+           An instance of the Trainingdata class
         """
         # Create a (geometry, classname, id) geodf of all classes geodf
         # shapefiles = gpd.GeoDataFrame(pd.concat(list_gdfs,ignore_index=True, sort=False))
@@ -85,7 +98,7 @@ class Trainingdata:
         geoms = gdf.geometry.values
 
         # extract the raster values within the polygon
-        with rasterio.open(path_to_raster) as src:
+        with rasterio.open(r_obj.path_to_raster) as src:
             X = np.array([]).reshape(0,src.count)# pixels for training
             y = np.array([], dtype=np.string_) # labels for training
             for index, geom in enumerate(geoms):
@@ -107,13 +120,45 @@ class Trainingdata:
                 X = np.vstack((X,out_image_reshaped))
 
         if write:
-            fname = os.path.splitext(os.path.basename(path_to_raster))[0]
-            if not outdir:
-                # set outdir as the input raster location
-                outdir = os.path.dirname(path_to_raster)
-            np.save(os.path.join(outdir,fname+'_features.npy'),X)
-            np.save(os.path.join(outdir,fname+'_lables.npy'),y)
+            np_to_disk(X, 'features', r_obj.path_to_raster, outdir=outdir)
+            np_to_disk(y, 'lables', r_obj.path_to_raster, outdir=outdir)
 
         return Trainingdata(X,y)
 
+    @staticmethod
+    def predict(r_obj, v_split, h_split, classifier, write=False, outdir=None):
+        """
+        Use an already optimized skitlearn classifier to classify pixels of
+        a raster object.
 
+        ************
+
+        params:
+            r_obj -> An istance of the Raster class
+            v_split -> number of times the raster will be divided
+                       along its height
+            h_split -> number of times the raster will be divided
+                       along its width
+            classifier -> optimised classifier
+            outdir -> full path to output directory
+
+        return:
+            class_final: A 2D numpy array of classnames with
+                         the same height and width of r_obj
+        """
+        # Allocate a numpy array for strings
+        class_final = np.empty((r_obj.height,r_obj.width), dtype="S10")
+
+        for row, col, patch in r_obj.get_patches(v_split, h_split):
+            np.nan_to_num(patch, copy=False,nan=0.0, posinf=0,neginf=0)
+            # Generate a prediction array
+            # cols: a single patch reshaped into a vector
+            # rows: bands of a single patch
+            class_prediction = classifier.predict(patch.reshape(-1, patch.shape[2]))
+            # Reshape our classification map back into a 2D matrix so we can visualize it
+            class_prediction = class_prediction.reshape(patch[:,:,0].shape)
+            # fill in the class_final with class names
+            class_final[row:row+patch.shape[0],col:col+patch.shape[0]] = class_prediction
+        if write:
+            np_to_disk(class_final, 'class_prediction', r_obj.path_to_raster, outdir=outdir)
+        return class_final
