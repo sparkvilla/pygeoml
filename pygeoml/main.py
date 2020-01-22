7cf658b6f671e6f63c9e44eb5cacaa400f4a1dbd
sparkvilla <diego.villamaina@gmail.com>
2020-01-22 15:51:03 +0100

    added workflow steps

--- pygeoml/main.py
+++ pygeoml/main.py
@@ -11,24 +11,12 @@ from parser import Sentinel2
 from shape import Shapeobj
 from train import Trainingdata
 
-
-if __name__ == "__main__":
-
-
-    basedir = '/home/diego/work/dev/ess_diego/Data_Diego'
-
-    # 2A-Level original data 10 m
-    datadir_10m = os.path.join(basedir,'Hanneke/S2A_MSIL2A_20191026T074011_N9999_R092_T37MBN_20191121T154707.SAFE/GRANULE/L2A_T37MBN_A022683_20191026T075457/IMG_DATA/R10m')
-    # 2A-Level original data 20 m
-    datadir_20m = os.path.join(basedir,'Hanneke/S2A_MSIL2A_20191026T074011_N9999_R092_T37MBN_20191121T154707.SAFE/GRANULE/L2A_T37MBN_A022683_20191026T075457/IMG_DATA/R20m')
-    # shape files
-    datadir_shp = os.path.join(basedir,'Hanneke/S2A_MSIL2A_20191026T074011_N9999_R092_T37MBN_20191121T154707.SAFE/field_points_dataframe')
-
-    # output directory
-    outdir = os.path.join(basedir,'Hanneke/output/S2A_MSIL2A_20191026T074011_N9999_R092_T37MBN_20191121T154707.SAFE_out')
-
+def step1(dir10, dir20, scl_name, outdir):
+    """
+    Create a cloud masked stack array
+    """
     # 1. create a cloud mask at 10m resol based on scf file
-    fpath_scl = os.path.join(datadir_20m,'T37MBN_20191026T074011_SCL_20m.jp2')
+    fpath_scl = os.path.join(dir20, scl_name)
     r_scf = Raster(fpath_scl)
     ## Upsample scene classification Raster image
     r_scf_resampled = Raster.resample_raster(r_scf, scale=2, outdir=outdir)
@@ -43,44 +31,120 @@ if __name__ == "__main__":
     arr_scf_resampled_mask = Raster.mask_arr_equal(arr_scf_resampled, [3,7,8,9,10])
     arr_scf_resampled_mask.dump(os.path.join(outdir,'mask_10m.npy'))
 
+    p = Sentinel2(dir10)
+    # get raster files
+    rfiles = [ i[0] for i in p.srfiles]
+
     # 2. create the stack
-    r_stack = Rastermsp.create_stack(datadir_10m, Sentinel2, outdir)
-    arr_stack = r_stack.load_as_arr()
-    # 3. create a masked stack
-    arr_stack_masked = r_stack.mask_arr(arr_stack, arr_scf_resampled_mask.mask, write=True, outdir=outdir)
+    r_stack = Rastermsp.create_stack(rfiles, outdir, arr_scf_resampled_mask.mask)
 
+def step2(dirshp, outdir):
+    """
+    Extract pixels at point location and create training data
+    """
+    stackdir = os.path.join(outdir,'multibands_masked.gtif')
+    stack = Rastermsp(stackdir)
+    pt = Shapeobj(dirshp)
+    train = Trainingdata.calc_xy(stack, pt.gdf, write=True, outdir=outdir)
+    train.add_class_xy("masked", 0)
+    # Esclude classes with samples less then 10
+    train.exclude_classes()
+    train.save_xy(stack.path_to_raster, outdir)
 
-
-
-
-    # Load raster
-#    fpath_red = os.path.join(datadir,'T37MBN_20190628T073621_B04_10m.jp2')
-#    r_red = Raster(fpath_red)
-#    poly = r_red.get_raster_polygon()
-
-    # Field measurments
-    #shpdir = os.path.join(outdir,'field_points_dataframe')
-   # stackdir = os.path.join(outdir,'multibands_masked.gtif')
-    #pt = Shapeobj(os.path.join(shpdir,'field_points_dataframe.shp'))
-    #field_pt.gdf_within_polygon(poly)
-    #field_pt.rename_field("Tree plantation", "Tree_plantation")
-    #field_pt.rename_field("Bare rock", "Bare_rock")
-    #field_pt.write_gdf(outdir=outdir)
-
-    #stack = Rastermsp(stackdir)
-
-    # get a mask for this dataset
-    #c_mask = np.load(os.path.join(outdir,'mask_10m'), allow_pickle=True)
-    # Mask stack
-    #stack_masked = stack.mask_arr(stack_arr, c_mask.mask, write=True, outdir=outdir)
-#    train = Trainingdata.calc_xy(stackdir, pt.gdf, write=True, outdir=outdir)
-   # train = Trainingdata.load_xy(os.path.join(outdir,'multibands_masked_features.npy'), os.path.join(outdir,'multibands_masked_lables.npy'))
-   # train.X = np.delete(train.X, 304, axis=0)
-   # train.y = np.delete(train.y, 304 )
-   # train.build_class_to_int_map()
-
-# Initialize our model with 400 trees
-    #rf_f = RandomForestClassifier(n_estimators=400, oob_score=True)
+def step3(outdir):
+    """
+    Make prediction on real data using a pre-optimized classifier
+    """
+    stackdir = os.path.join(outdir,'multibands_masked.gtif')
+    stack = Rastermsp(stackdir)
+    train = Trainingdata.load_xy(os.path.join(outdir,'multibands_masked_features.npy'), os.path.join(outdir,'multibands_masked_lables.npy'))
+    #  Initialize our model with 400 trees
+    rf_f = RandomForestClassifier(n_estimators=800, oob_score=True)
     # Fit our model to training data
-    #rf_f = rf_f.fit(train.X, train.y)
-    ####class_prediction = Trainingdata.predict(stack, 3, 3, rf_f, write=True, outdir=outdir)
+    rf_f = rf_f.fit(train.X, train.y)
+    class_prediction = Trainingdata.predict(stack, 3, 3, rf_f, write=True, outdir=outdir)
+
+
+def plot_class_prediction():
+    class_prediction = np.load(os.path.join(outdir,'multibands_masked_class_prediction.npy'))
+
+    # Transform string class in integer class
+
+    def str_class_to_int(class_array):
+        class_array[class_array == b'Agricultur'] = 1
+        class_array[class_array == b'Bare_rock'] = 2
+        class_array[class_array == b'Building'] = 3
+        class_array[class_array == b'CharcoalKi'] = 4
+        class_array[class_array == b'Forest'] = 5
+        class_array[class_array == b'Livestock'] = 6
+        class_array[class_array == b'River'] = 7
+        class_array[class_array == b'Road'] = 8
+        class_array[class_array == b'masked'] = 9
+        return(class_array.astype(int))
+
+    class_prediction = str_class_to_int(class_prediction)
+
+    # find the highest pixel value in the prediction image
+    n = int(np.max(class_prediction))
+    # next setup a colormap for our map
+    colors = dict((
+        (0, (218, 165, 32)),   # golden rod - Agri
+        (1, (188, 143, 143)),  # rosy brown - Rock
+        (2, (119, 136, 153)),  # light slate gray - Building
+        (3, (139, 69, 19)),    # saddle brown  - Charcol
+        (4, (34, 139, 34)),    # forest green - forest
+        (5, (255, 255, 0)),    # yellow - livestock
+        (6, (127, 255, 212)),  # acqua marine - river
+        (7, (178, 34, 34)),    # firebrick - road
+        (8, (255, 255, 255)),  # White - masked
+    ))
+
+    # Transform 0 - 255 color values from colors as float 0 - 1
+    for k in colors:
+        v = colors[k]
+        _v = [_v / 255.0 for _v in v]
+        colors[k] = _v
+    index_colors = [colors[key] for key in range(0, n )]
+
+    cmap = plt.matplotlib.colors.ListedColormap(index_colors, 'Classification', n)
+
+
+    from matplotlib.patches import Patch
+    # Create a list of labels to use for your legend
+    class_labels = ['Agricultur', 'Bare_rock', 'Building', 'CharcoalKi', 'Forest',
+           'Livestock', 'River', 'Road', 'masked']
+    # A path is an object drawn by matplotlib. In this case a patch is a box draw on your legend
+    # Below you create a unique path or box with a unique color - one for each of the labels above
+    legend_patches = [Patch(color=icolor, label=label)
+                      for icolor, label in zip(index_colors, class_labels)]
+
+    # Plot Classification
+    fig, axs = plt.subplots(1,1,figsize=(30,30))
+    axs.imshow(class_prediction, cmap=cmap, interpolation='none')
+    axs.legend(handles=legend_patches,
+              facecolor="white",
+              edgecolor="white",
+              bbox_to_anchor=(1.35, 1),
+              fontsize=30)  # Place legend to the RIGHT of the map
+    axs.set_axis_off()
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    basedir = '/home/diego/work/dev/ess_diego/Data_Diego'
+
+    # 2A-Level original data 10 m
+    datadir_10m = os.path.join(basedir,'Hanneke/S2A_MSIL2A_20190906T073611_N9999_R092_T37MBN_20191121T152023.SAFE/GRANULE/L2A_T37MBN_A021968_20190906T075543/IMG_DATA/R10m')
+    # 2A-Level original data 20 m
+    datadir_20m = os.path.join(basedir,'Hanneke/S2A_MSIL2A_20190906T073611_N9999_R092_T37MBN_20191121T152023.SAFE/GRANULE/L2A_T37MBN_A021968_20190906T075543/IMG_DATA/R20m')
+    # shape files
+    datadir_shp = os.path.join(basedir,'Hanneke/S2A_MSIL2A_20190906T073611_N9999_R092_T37MBN_20191121T152023.SAFE/field_points_dataframe')
+
+    # output directory
+    outdir = os.path.join(basedir,'Hanneke/output/S2A_MSIL2A_20190906T073611_N9999_R092_T37MBN_20191121T152023.SAFE_out')
+
+
+    #step1(datadir_10m, datadir_20m, 'T37MBN_20190906T073611_SCL_20m.jp2', outdir)
+    step2(datadir_shp, outdir)
+    #plot_class_prediction()
